c21b0b3ac756228fcadd7706012a84545b6eb8cb
Lukas Lotz <11498557+llotz@users.noreply.github.com>
2020-12-07 20:24:37 +0100

    finally fixed day 4

--- 4.py
+++ 4.py
@@ -27,13 +27,13 @@ for line in lines:
 					optionsValid += 1
 				if f[0]=="hgt" and ((val.endswith("cm") and int(val[0:-2]) <= 193 and int(val[0:-2]) >= 150) or (val.endswith("in") and int(val[0:-2]) <= 76 and int(val[0:-2]) >= 59)):
 					optionsValid += 1
-				if f[0]=="hcl" and re.match(r"#[a-fA-F0-9]{6}", val):
+				if f[0]=="hcl" and re.match(r"^#[a-f0-9]{6}$", val):
 					optionsValid += 1
 				if f[0]=="ecl" and val in ["amb","blu", "brn" ,"gry", "grn", "hzl", "oth"]:
 					optionsValid += 1
-				if f[0]=="pid" and re.match(r"[0-9]{9}", val):
+				if f[0]=="pid" and re.match(r"^[0-9]{9}$", val):
 					optionsValid += 1
 			print(optionsValid);
 			if optionsValid == 7:		
 				passedPasses += 1
-print(passedPasses)
+print("completed: ",passedPasses)
